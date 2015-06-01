dd3a11ce44b042317666edab92c20cc360e17962
twalder-docnet <tom@doc-net.com>
2015-06-01 12:21:32 +0100

    Moving examples

---
+++ examples/matt.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Examples for MATT client
+ */
+
+require(dirname(__FILE__) . '/../src/Docnet/MATT.class.php');
+
+// This should happen once per day
+Docnet\MATT::expect('Google upload OK [test]')->every('day');
+
+// Something we expect to happen at least every 15 minutes
+Docnet\MATT::expect('Order placed [test]')->every('15m')->sms('07715122253');
+
+// This thing should NEVER happen
+Docnet\MATT::expect('Horrible error [test]')->never()->email('tom');
+
